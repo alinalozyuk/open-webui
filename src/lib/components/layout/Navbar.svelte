0e139406dc637bafce70ff9c67a6c07503fea745
Alina Lozyuk <156336826+alinalozyuk@users.noreply.github.com>
2025-01-22 14:59:35 +0100

    Update Navbar.svelte

--- src/lib/components/layout/Navbar.svelte
+++ src/lib/components/layout/Navbar.svelte
@@ -75,9 +75,7 @@
 			{$showSidebar ? 'ml-1' : ''}
 			"
 			>
-				{#if showModelSelector}
-					<ModelSelector bind:selectedModels showSetDefault={!shareEnabled} />
-				{/if}
+	
 			</div>
 
 			<div class="self-start flex flex-none items-center text-gray-600 dark:text-gray-400">
